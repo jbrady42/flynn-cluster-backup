67576f55ac82afb26f6ed306e2a2d77ee0156587
Jason Brady <jabrady42@gmail.com>
2017-08-27 14:30:44 -0500

    Don't run backup on start. Add now flag to run single backup

--- src/runner.go
+++ src/runner.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 	"os/exec"
 	"time"
 )
 
+var now = flag.Bool("now", false, "Run backup now and exit")
+
 func main() {
+	flag.Parse()
 	configFlynn()
-	watchBackup()
+
+	if *now {
+		runBackup()
+	} else {
+		watchBackup()
+	}
 }
 
 func configFlynn() {
@@ -37,11 +46,11 @@ func watchBackup() {
 	log.Println("Start backup worker")
 	for true {
 		startHour := currnetHour()
-		runBackup()
-		// Wait for next hour
+		// Wait for start of next hour
 		for tmp := startHour; tmp == startHour; tmp = currnetHour() {
 			time.Sleep(1 * time.Minute)
 		}
+		runBackup()
 	}
 }
 
